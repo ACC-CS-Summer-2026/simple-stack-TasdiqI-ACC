5d8ca04241c3e9a1751b0d3c31417fecd25169e2
Tasdiq Islam <tasdiq.islam@g.austincc.edu>
2026-06-19 16:16:07 -0500

    Added main header directives for stack testing driver

--- main.h
+++ main.h
@@ -4,3 +4,13 @@
 *  Purpose: Contains declarations and definitions
 *           needed by the main driver file.
 ************************************************* */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include "stack.h"
+
+#endif //MAIN_H
