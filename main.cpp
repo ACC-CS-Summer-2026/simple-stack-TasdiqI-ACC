63f5a29011ca4d4806db5a742085b33df2db6153
Tasdiq Islam <tasdiq.islam@g.austincc.edu>
2026-06-19 16:19:48 -0500

    Added initial main driver test for empty stack state

--- main.cpp
+++ main.cpp
@@ -4,3 +4,23 @@
 *  Purpose: Contains the driver and testing code
 *           for the stack ADT.
 ************************************************* */
+
+#include "main.h"
+
+int main() {
+    /* *********************************
+    * Tests all stack operations and states.
+    *
+    * @param na : na
+    * @return int : application exit code
+    * @exception na : na
+    * @note na
+    * *********************************/
+
+    Stack stack;
+
+    std::cout << "New stack created." << std::endl;
+    std::cout << "Stack empty: " << stack.isEmpty() << std::endl;
+
+    return 0;
+}
